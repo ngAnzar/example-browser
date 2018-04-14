99bb0dee2fddbac3dd55a4465d32f7cfc9889a08
Vetési Zoltán <vetesi.zoltan@gmail.com>
2018-04-14 22:43:07 +0200

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+import path from "path"
+import webpack from "webpack"
+import HtmlWebpackPlugin from "html-webpack-plugin"
+import { config, environment } from "@anzar/build"
+
+
+export function root(location) {
+    return path.resolve(__dirname, location)
+}
+
+
+export default config("@anzar/build-browser", {
+    entry: {
+        polyfills: root("src/polyfills.ts"),
+        app: root("src/app.module.ts")
+    }
+})
